7369707c946e0f7761282b120cb794e6223b7246
dmitruz <dmitruz970@gmail.com>
2023-01-31 16:31:50 +0200

    feature: create input form

---
+++ RestaurantMenu/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <input type="text" class="rounded border-2" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+const meals = computed(() => store.state.meals);
+</script>
+
+<style lang="scss" scoped></style>
