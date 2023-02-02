820d0f0ff72991febb09a04042e6ace3eefb51ce
dmitruz <dmitruz970@gmail.com>
2023-02-02 21:11:43 +0200

    request url

--- RestaurantMenu/src/views/Home.vue
+++ RestaurantMenu/src/views/Home.vue
@@ -21,11 +21,16 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import store from '../store';
+import axiosClient from '../axiosClient.js';
 
-const meals = computed(() => store.state.meals);
 const letters = 'ABCDEFGHIJKLMNOPQRTUVWXYZ'.split('');
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list');
+  console.log(response.data);
+});
 </script>
 
 <style lang="scss" scoped></style>
