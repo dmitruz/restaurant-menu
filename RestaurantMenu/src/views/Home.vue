c2c5720ef788b166e3139884b614143ef2eb5627
dmitruz <dmitruz970@gmail.com>
2023-02-03 12:34:13 +0200

    created melas views

--- RestaurantMenu/src/views/Home.vue
+++ RestaurantMenu/src/views/Home.vue
@@ -21,15 +21,17 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import store from '../store';
 import axiosClient from '../axiosClient.js';
 
 const letters = 'ABCDEFGHIJKLMNOPQRTUVWXYZ'.split('');
+const ingredients = ref([]);
 
 onMounted(async () => {
   const response = await axiosClient.get('/list.php?i=list');
   console.log(response.data);
+  ingredients.value = response.data;
 });
 </script>
 
